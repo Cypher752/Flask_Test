30ca84d993343353038d3927b69766e221d1ad0f
Cypher752 <pzelaskowski@gmx.de>
2023-09-01 12:39:52 +0200

    Changes to be committed: 	modified:   .DS_Store 	modified:   app.py 	modified:   dockerfile

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import desc 
+from sqlalchemy import desc, update
 from datetime import datetime
 from flask import Flask, render_template, request, send_from_directory, url_for, redirect
-from sqlalchemy import update
 import os 
 
 app = Flask(__name__)
