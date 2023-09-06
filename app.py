3ca98b77073bae2c865d2320022becb676b8dda1
Cypher752 <142492413+Cypher752@users.noreply.github.com>
2023-09-06 12:51:43 +0200

    add sqlpostgre

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import os
 
 app = Flask(__name__)
 
-username =' ' """
+username =' ' 
+"""
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
